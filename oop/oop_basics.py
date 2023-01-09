e627e62a5144fa7dc413255b831a7dcb5f0ef9f6
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-10 01:21:11 +0530

    php

--- oop/oop_basics.py
+++ oop/oop_basics.py
@@ -1,23 +1,48 @@
-class Item:
-
-    # def __init__(self) -> None:
-    #     pass
-
-    def __init__(self, name, price, number):
-        print("hello")
-        self.name = name
-        self.price = price
-        self.number = number
-
-    def cal_total_price(self):
-        print(self.price * self.number)
+# from ast import Assert
 
 
-item1 = Item("anand", 100, 45)
+# class Item:
 
-item1.cal_total_price()
+#     # def __init__(self) -> None:
+#     #     pass
+#     pay_rate=0.8 
+#     # 
+#     def __init__(self, name:str, price:float, number:int):
+        
+#         # all class methods need to recieve a self argument as it is passed
+#         # by default in the call statement
+        
+        
+#         assert price>=0,f"price not greater than zero  {price}"
+#         assert number>=0
+        
+#         # assert statements are used to assert conditions upon receiving data
+        
+#         print("hello")
+#         self.name = name
+#         self.price = price
+#         self.number = number
 
 
-item2 = Item('hello', 1234, 5)
 
-item2.cal_total_price()
+#     def cal_total_price(self):
+#         print(self.price * self.number)
+
+
+# item1 = Item("anand", -100, 45)
+
+# item1.cal_total_price()
+
+
+# item2 = Item('hello', 1234, 5)
+
+# item2.cal_total_price()
+
+
+
+n = 10
+nb=len(bin(n))-2
+for i in range(0,n+1):
+    li=nb-(len(bin(i))-2)
+    d=bin(i)
+    print('a'*(li),d[2:],sep='')
