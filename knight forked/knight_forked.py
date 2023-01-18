984ab20a365eeca2b3a9c97ecdd08fbfb7fc8ec8
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-19 01:24:47 +0530

    it workssss!! ima go cry

--- knight forked/knight_forked.py
+++ knight forked/knight_forked.py
@@ -1,38 +1,32 @@
-# board=[[0,1,2,3,4,5,6,7],
-# [0,1,2,3,4,5,6,7],
-# [0,1,2,3,4,5,6,7],
-# [0,1,2,3,4,5,6,7],
-# [0,1,2,3,4,5,6,7],
-# [0,1,2,3,4,5,6,7],
-# [0,1,2,3,4,5,6,7],
-# [0,1,2,3,4,5,6,7]]
+# board showing co-ordinates used in calculation
 
-# print(len(board))
-
-
-board=[0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17,
-        20, 21, 22, 23, 24, 25, 26, 27, 30, 31, 32, 33, 34, 35, 36, 37, 
-        40, 41, 42, 43, 44, 45, 46, 47, 50, 51, 52, 53, 54, 55, 56, 57, 
-        60, 61, 62, 63, 64, 65, 66, 67, 70, 71, 72, 73, 74, 75, 76, 77]
+board=[0, 1, 2, 3, 4, 5, 6, 7, 
+       10, 11, 12, 13, 14, 15, 16, 17,
+       20, 21, 22, 23, 24, 25, 26, 27, 
+       30, 31, 32, 33, 34, 35, 36, 37, 
+       40, 41, 42, 43, 44, 45, 46, 47, 
+       50, 51, 52, 53, 54, 55, 56, 57, 
+       60, 61, 62, 63, 64, 65, 66, 67, 
+       70, 71, 72, 73, 74, 75, 76, 77]
 
 
-
-start_position=22
-
+start_position=eval(input("enter starting position "))
 
 def possible_positions(position):
     num=[8,-8,12,-12,19,-19,21,-21]
     tem=[]  #array for holding all possible positions ie 8 of them
     for i in num:
         tem.append(position+i)
-    return(list(i for i in tem if i <78 and i>=0))
+    return(list(i for i in tem if i in board))
+
+
 
 moves_required=0
 output_dict={start_position:moves_required}
 
 
 
-while (len(output_dict)<63):
+while (len(output_dict)<64):
     moves_required+=1
     searched_positions=list(output_dict.keys())
     for i in searched_positions:
@@ -41,9 +35,26 @@ while (len(output_dict)<63):
             if x not in list(output_dict.keys()):
                 output_dict[x]=moves_required
                 
-print(output_dict)
+
+# output_dict is your result, the rest is for sorting the dictionary
+
+myKeys = list(output_dict.keys())
+myKeys.sort()
+sorted_dict = {i: output_dict[i] for i in myKeys}
+# print(sorted_dict)
 
 
+# function for pretifying the dictionary into a neat list
+def pretty(l):
+    t=[]
+    for i in range(0,63,8):
+        t.append(l[i:i+8])
+    for i in t:
+        print(i)
+    
+    
+pretty(list(sorted_dict.values()))
+        
 
 
 
