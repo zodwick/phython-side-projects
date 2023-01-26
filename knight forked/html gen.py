0c2304b9e4c882cfac609ab059b753ecfc97fbca
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-27 01:10:27 +0530

    swayy

--- knight forked/html gen.py
+++ knight forked/html gen.py
@@ -1,7 +1,8 @@
-aa='''<button className={'_'+ot_values[start_pos][0]} onClick={() => setstart_pos(0)}>
-{ot_values[start_pos][0]}
-</button>'''
+# aa='''<button className={'_'+ot_values[start_pos][0]} onClick={() => setstart_pos(0)}>
+# {ot_values[start_pos][0]}
+# </button>'''
     
     
-for i in range(0,64):
-    print(aa.replace('0',str(i)))
+# for i in range(0,64):
+#     print(aa.replace('0',str(i)))
+
