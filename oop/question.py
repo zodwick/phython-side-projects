a38277c68a696ee0b4ae38d89535cf5b9a0e5fca
zodwick <115868723+zodwick@users.noreply.github.com>
2023-02-28 00:27:14 +0530

    not too shaby

--- oop/question.py
+++ oop/question.py
@@ -51,6 +51,41 @@ def id(n):
        
     
     
-id(4)
+
     
     
+
+
+
+
+import math
+def prime(a):
+    if (a % 2 == 0) and (a > 2): 
+        return False
+    return all(a % i for i in range(3, int(math.sqrt(a)) + 1, 2))
+
+primes = ''
+
+for i in range(2,21000):
+    if len(primes) < 10000:
+        if prime(i):
+            primes = primes + str(i)
+    else:
+        break
+
+def solution(a):
+    reid = primes[a:a+5:1]
+    print(reid)
+
+solution(9000)
+
+def timeit(func):
+    import time
+    start = time.time()
+    func()
+    end = time.time()
+    print(func.__name__,end - start)
+    
+    
+timeit(lambda :solution(9000))
+timeit(lambda :id(9000))
